27d33f7e449f909ab96b1b88bc61933c1ff051c1
Nicolas Guillen <nicolas.guillen@ocean.ibm.com>
2022-06-08 22:28:09 -0600

    🦍 auth0 login implementation, no guards or user load, see docs quickstart

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,6 +20,8 @@ import { User, Sun, MoonStars } from "tabler-icons-react";
 
 import BottomNavigation from "../components/BottomNavigation";
 
+import { UserProvider } from "@auth0/nextjs-auth0";
+
 const useStyles = createStyles((theme) => ({
   collectionLink: {
     display: "block",
@@ -59,89 +61,100 @@ export default function App(props: AppProps) {
   const theme = useMantineTheme();
 
   return (
-    <ColorSchemeProvider
-      colorScheme={colorScheme}
-      toggleColorScheme={toggleColorScheme}
-    >
-      <Head>
-        <title>Page title</title>
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width"
-        />
-      </Head>
-
-      <MantineProvider
-        withGlobalStyles
-        withNormalizeCSS
-        theme={{
-          colorScheme,
-          colors: {
-            'bright-yellow': ['#FFD43B','#FFD43B','#FFD43B','#FFD43B','#FFD43B','#FFD43B','#FFD43B','#FFD43B']
-          }
-        }}
+    <UserProvider>
+      <ColorSchemeProvider
+        colorScheme={colorScheme}
+        toggleColorScheme={toggleColorScheme}
       >
-        <AppShell
-          styles={{
-            body: { minHeight: "100vh", paddingTop: "50px"},
-          }}
-          padding={0}
-          header={
-            <Header height={50} p="xs" style={{ borderBottom: "none" }} fixed>
-              <Burger
-                size={14}
-                opened={opened}
-                onClick={() => setOpened((o) => !o)}
-                title={title}
-              />
-            </Header>
-          }
-        >
-          <Component {...pageProps} />
-        </AppShell>
-        <BottomNavigation />
-        <Drawer
-          position="bottom"
-          opened={opened}
-          onClose={() => setOpened(false)}
-          title="Toptierperk"
-          padding="xl"
-        >
-          <ScrollArea style={{ height: 250 }}>
-            <Stack justify="flex-start" spacing="xs">
-              <Anchor
-                href="https://mantine.dev/"
-                target="_blank"
-                component="a"
-                underline={false}
-                className={classes.collectionLink}
-              >
-                <User size={14} style={{ marginRight: 9 }} />
-                Home
-              </Anchor>
-              <Anchor
-                href="https://mantine.dev/"
-                target="_blank"
-                component="a"
-                underline={false}
-                className={classes.collectionLink}
-              >
-                <User size={14} style={{ marginRight: 9 }} />
-                Casa
-              </Anchor>
+        <Head>
+          <title>Page title</title>
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width"
+          />
+        </Head>
 
-              <ActionIcon
-                variant="outline"
-                color={dark ? "yellow" : "blue"}
-                onClick={() => toggleColorScheme()}
-                title="Toggle color scheme"
-              >
-                {dark ? <Sun size={18} /> : <MoonStars size={18} />}
-              </ActionIcon>
-            </Stack>
-          </ScrollArea>
-        </Drawer>
-      </MantineProvider>
-    </ColorSchemeProvider>
+        <MantineProvider
+          withGlobalStyles
+          withNormalizeCSS
+          theme={{
+            colorScheme,
+            colors: {
+              "bright-yellow": [
+                "#FFD43B",
+                "#FFD43B",
+                "#FFD43B",
+                "#FFD43B",
+                "#FFD43B",
+                "#FFD43B",
+                "#FFD43B",
+                "#FFD43B",
+              ],
+            },
+          }}
+        >
+          <AppShell
+            styles={{
+              body: { minHeight: "100vh", paddingTop: "50px" },
+            }}
+            padding={0}
+            header={
+              <Header height={50} p="xs" style={{ borderBottom: "none" }} fixed>
+                <Burger
+                  size={14}
+                  opened={opened}
+                  onClick={() => setOpened((o) => !o)}
+                  title={title}
+                />
+              </Header>
+            }
+          >
+            <Component {...pageProps} />
+          </AppShell>
+          <BottomNavigation />
+          <Drawer
+            position="bottom"
+            opened={opened}
+            onClose={() => setOpened(false)}
+            title="Toptierperk"
+            padding="xl"
+          >
+            <ScrollArea style={{ height: 250 }}>
+              <Stack justify="flex-start" spacing="xs">
+                <Anchor
+                  href="https://mantine.dev/"
+                  target="_blank"
+                  component="a"
+                  underline={false}
+                  className={classes.collectionLink}
+                >
+                  <User size={14} style={{ marginRight: 9 }} />
+                  Home
+                </Anchor>
+                <Anchor
+                  href="https://mantine.dev/"
+                  target="_blank"
+                  component="a"
+                  underline={false}
+                  className={classes.collectionLink}
+                >
+                  <User size={14} style={{ marginRight: 9 }} />
+                  Casa
+                </Anchor>
+
+                <ActionIcon
+                  variant="outline"
+                  color={dark ? "yellow" : "blue"}
+                  onClick={() => toggleColorScheme()}
+                  title="Toggle color scheme"
+                >
+                  {dark ? <Sun size={18} /> : <MoonStars size={18} />}
+                </ActionIcon>
+              </Stack>
+            </ScrollArea>
+          </Drawer>
+        </MantineProvider>
+      </ColorSchemeProvider>
+    </UserProvider>
   );
 }
