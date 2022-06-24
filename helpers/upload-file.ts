593ce8768b0c92d9effed4e7975c29ec92fb3b88
nicolascalev <nicolascalevg@gmail.com>
2022-06-24 12:05:16 -0600

    🐱‍👤 handle file uploads

---
+++ helpers/upload-file.ts
@@ -0,0 +1,67 @@
+import fs from "fs"
+import AWS from "aws-sdk"
+
+const s3 = new AWS.S3({
+  endpoint: process.env.DO_SPACES_URL,
+  region: process.env.DO_SPACES_REGION,
+  credentials: {
+    accessKeyId: process.env.DO_SPACES_ID!,
+    secretAccessKey: process.env.DO_SPACES_SECRET!,
+  }
+})
+
+type CustomFile = {
+  lastModifiedDate?: Date;
+  filepath?: string;
+  newFilename?: string;
+  originalFilename?: string;
+  mimetype?: string;
+  hashAlgorithm?: boolean;
+  size?: number;
+}
+
+type Uploads = {
+  errors: Error[],
+  success: any[]
+}
+
+async function uploadFile(files: CustomFile[], folder = '') : Promise<Uploads> {
+  // creamos uuna promesa ya que vamos a ejecutar un callback varias veces
+  return new Promise(resolve => {
+    s3.createBucket(() => {
+      // definimos la variable que va a tener la respuesta
+      var awsUpload: Uploads = {
+        errors: [],
+        success: []
+      };
+
+      // recorremos el arreglo de archivos que debemos subir
+      for (let i = 0; i < files.length; i++) {
+        const file = files[i];
+
+        const config = {
+          Key: folder + Date.now() + file.originalFilename!,
+          Bucket: process.env.DO_SPACES_BUCKET!,
+          Body: fs.createReadStream(file.filepath!),
+          ACL: "public-read",
+        };
+
+        // subimos el archivo con la configuracion, si hay error lo annadimos al arreglo y si si no tambien
+        s3.upload(config, (err: Error, data: any) => {
+          if (err) {
+            awsUpload.errors.push(err);
+          } else {
+            awsUpload.success.push(data);
+          }
+
+          // si ya se recorrio todo el arreglo entonces devuelva el resultado
+          if (i === files.length - 1) {
+            resolve(awsUpload);
+          }
+        });
+      }
+    });
+  });
+}
+
+export default uploadFile
