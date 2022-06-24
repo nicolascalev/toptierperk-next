8725e174d74269d7006cd2d3120c558ca04b51d8
nicolascalev <nicolascalevg@gmail.com>
2022-06-24 12:03:12 -0600

    📦 use aws, formiddable for file uploads, add Digital Ocean params, and allowed origins for photos

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['devttp.sfo3.digitaloceanspaces.com'],
+  },
 }
 
 module.exports = nextConfig
