58577aabf1c3e75be733535b5776401d4a20f7ca
Nicolas Guillen <guillen_nicolas@live.com>
2022-06-24 12:07:13 -0600

    🦍 get files to upload for benefit, and pass them to model

--- pages/api/benefit/index.ts
+++ pages/api/benefit/index.ts
@@ -3,6 +3,14 @@ import { getSession } from "@auth0/nextjs-auth0";
 import Benefit, { BenefitCreateParams } from "../../../prisma/models/Benefit";
 import Joi from "joi";
 import isAuthenticated from "../../../helpers/isAuthenticated";
+import parseFormData from "../../../helpers/parse-form-data";
+import uploadFile from "../../../helpers/upload-file";
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
 
 const createBenefitSchema = Joi.object({
   name: Joi.string().required(),
@@ -16,46 +24,55 @@ const createBenefitSchema = Joi.object({
   isActive: Joi.boolean(),
 });
 
+type FilesNextApiRequest = NextApiRequest & { files?: any };
+
 export default async function userHandler(
-  req: NextApiRequest,
-  res: NextApiResponse,
+  req: FilesNextApiRequest,
+  res: NextApiResponse
 ) {
-  const { method, body } = req;
+  // POST /api/benefit
+  if (req.method == "POST") {
+    try {
+      await isAuthenticated(req, res);
+      await parseFormData(req, res);
+      const { body, files } = req;
+      const session = getSession(req, res);
 
-  switch (method) {
-    case "POST":
-      try {
-        await isAuthenticated(req, res)
-        const session = getSession(req, res)
+      const data: BenefitCreateParams = {
+        name: body.name,
+        description: body.description,
+        categories: body.categories ? body.categories.split(",") : [],
+        supplier: session!.user.id,
+        beneficiaries: body.beneficiaries
+          ? body.beneficiaries.split(",").map((id: string) => Number(id))
+          : [],
+        availableFor: body.availableFor
+          ? body.availableFor.split(",").map((id: string) => Number(id))
+          : [],
+        isPrivate: body.isPrivate,
+        isActive: body.isActive,
+      };
 
-        const data: BenefitCreateParams = {
-          name: body.name,
-          description: body.description,
-          categories: body.categories ? body.categories.split(",") : [],
-          // TODO: upload the pictures first to then pass them and connect them
-          photos: [],
-          supplier: session!.user.id,
-          beneficiaries: body.beneficiaries
-            ? body.beneficiaries.split(",").map((id: string) => Number(id))
-            : [],
-          availableFor: body.availableFor
-            ? body.availableFor.split(",").map((id: string) => Number(id))
-            : [],
-          isPrivate: body.isPrivate,
-          isActive: body.isActive,
-        };
-        const { value: validatedData, error: validationError } =
-          createBenefitSchema.validate(data);
-        if (validationError) {
-          return res.status(400).json({ error: validationError });
-        }
-        const result = await Benefit.create(validatedData);
-        res.status(200).json(result);
-      } catch (err) {
-        res.status(500).json(err);
+      const { value: validatedData, error: validationError } =
+        createBenefitSchema.validate(data);
+      if (validationError) {
+        return res.status(400).json({ error: validationError });
       }
-      break;
-    default:
-      res.status(400).json({ message: "Method or endpoint not implemented." });
+
+      // make sure that photos is always an array even if empty
+      const photos = files.photos.length ? files.photos : [files.photos];
+      const today = new Date().toLocaleDateString().split("/").join("-");
+      const upload = await uploadFile(photos, `benefits/${today}/`);
+      const uploadedPhotos = upload.success.map((up) => ({ url: up.Location }));
+      validatedData.photos = uploadedPhotos;
+
+      const result = await Benefit.create(validatedData);
+      return res.status(200).json(result);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
   }
+
+  res.status(405).json({ error: "Method or endpoint not implemented." });
 }
