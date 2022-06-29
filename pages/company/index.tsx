35d4826e104a3f54ba18fcacee87fffaaffbb42e
nicolascalev <nicolascalevg@gmail.com>
2022-06-29 16:43:51 -0600

    🚧 add stats to company page

--- pages/company/index.tsx
+++ pages/company/index.tsx
@@ -1,6 +1,13 @@
 import type { NextPage } from "next";
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
-import { useMantineTheme, Image, Text, Paper, Tabs } from "@mantine/core";
+import {
+  useMantineTheme,
+  Image,
+  Text,
+  Paper,
+  Tabs,
+  SimpleGrid,
+} from "@mantine/core";
 import { useEffect, useState } from "react";
 import { useWindowScroll } from "react-use";
 import { debounce } from "lodash";
@@ -30,11 +37,13 @@ interface Props {
 
 const Company: NextPage<Props> = ({ user }) => {
   const theme = useMantineTheme();
-  const { detailsOpacity, logoClass } = useDisplayProfile();
+  const { logoClass, showCompanyInfo } = useDisplayProfile();
 
   if (!user.company) {
     return <div>You have to be a part of a company or create one</div>;
   }
+
+  console.log(user)
 
   return (
     <div style={{ position: "relative", height: "200vh" }}>
@@ -45,9 +54,23 @@ const Company: NextPage<Props> = ({ user }) => {
             top: "52px",
             display: "flex",
             justifyContent: "center",
-            backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white,
+            backgroundColor:
+              theme.colorScheme === "dark" ? theme.colors.dark[7] : theme.white,
           }}
         >
+          {/* this div reduces impact of stats on scroll for now */}
+          <div
+            style={{
+              width: "100%",
+              height: "34px",
+              position: "absolute",
+              top: "0px",
+              backgroundColor:
+                theme.colorScheme === "dark"
+                  ? theme.colors.dark[7]
+                  : theme.white,
+            }}
+          ></div>
           <Image
             src={user.company.logo?.url}
             alt={user.company.name + " Toptierperk"}
@@ -55,22 +78,55 @@ const Company: NextPage<Props> = ({ user }) => {
           ></Image>
         </div>
       )}
-      <div
-        style={{
-          padding: theme.spacing.md,
-          textAlign: "center",
-          display: "flex",
-          flexWrap: "wrap",
-          opacity: detailsOpacity,
-        }}
-      >
-        <div style={{ width: "100%" }}>
-          <Text size="xl" weight={500}>
-            {user.company.name}
-          </Text>
-          <Paper>Perks - Offers - Employees</Paper>
+
+      {showCompanyInfo && (
+        <div
+          style={{
+            padding: theme.spacing.md,
+            textAlign: "center",
+            display: "flex",
+            flexWrap: "wrap",
+          }}
+        >
+          <div style={{ width: "100%" }}>
+            <Text size="xl" weight={500}>
+              {user.company.name}
+            </Text>
+            <Paper
+              radius="md"
+              p="md"
+              style={{
+                backgroundColor:
+                  theme.colorScheme === "dark"
+                    ? theme.colors.dark[6]
+                    : theme.colors.gray[0],
+                marginTop: theme.spacing.md,
+              }}
+            >
+              <SimpleGrid cols={3}>
+                <div>
+                  <Text weight="bold" size="lg">
+                    500
+                  </Text>
+                  <Text>Perks</Text>
+                </div>
+                <div>
+                  <Text weight="bold" size="lg">
+                    32
+                  </Text>
+                  <Text>Offers</Text>
+                </div>
+                <div>
+                  <Text weight="bold" size="lg">
+                    1000
+                  </Text>
+                  <Text>Claims</Text>
+                </div>
+              </SimpleGrid>
+            </Paper>
+          </div>
         </div>
-      </div>
+      )}
       <div
         style={{
           position: "sticky",
@@ -79,11 +135,12 @@ const Company: NextPage<Props> = ({ user }) => {
         }}
       >
         <Tabs grow variant="pills" position="apart" color="primary">
+          {/* TODO: show public and private just add a filter somewhere OR add the private instead of popular for mvp given that popular might be hard to calculate and make performant */}
           <Tabs.Tab label="Newest">
             <div>First tab content</div>
           </Tabs.Tab>
-          <Tabs.Tab label="Exclusive">Second tab content</Tabs.Tab>
           <Tabs.Tab label="Popular">Third tab content</Tabs.Tab>
+          <Tabs.Tab label="About">About tab content</Tabs.Tab>
         </Tabs>
       </div>
     </div>
