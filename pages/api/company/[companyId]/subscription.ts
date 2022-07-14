bdb09da379b19dfcd721f74b109f4f85be21bd28
Nicolas Guillen <guillen_nicolas@live.com>
2022-07-14 17:55:44 -0600

    🔒 add isAdmin policy

--- pages/api/company/[companyId]/subscription.ts
+++ pages/api/company/[companyId]/subscription.ts
@@ -2,21 +2,30 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import Company from "../../../../prisma/models/Company";
 import { getSession } from "@auth0/nextjs-auth0";
 import isAuthenticated from "../../../../helpers/isAuthenticated";
+import isAdmin from "../../../../helpers/isAdmin";
 
 export default async function findCompanyByNameHandler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   if (req.method === "PATCH") {
-    await isAuthenticated(req,res)
-    // TODO: add isAdmin validation
+    await isAuthenticated(req, res);
+    await isAdmin(req, res);
+
     let session = getSession(req, res);
 
     // TODO: if necessary integrate last payment date
-    const companyId = Number(req.query.companyId)
-    const subscriptionId = req.body.subscriptionId
+    const companyId = Number(req.query.companyId);
+    const subscriptionId = req.body.subscriptionId;
+    // The user can't be trying to update a company who does not belong to them
+    if (subscriptionId !== session!.user.adminOf.id) {
+      return res.status(401).send("Unauthorized");
+    }
     try {
-      const result = await Company.updateSubscription(companyId, subscriptionId);
+      const result = await Company.updateSubscription(
+        companyId,
+        subscriptionId
+      );
       session!.user = {
         ...session!.user,
         ...{
@@ -26,13 +35,11 @@ export default async function findCompanyByNameHandler(
       };
       return res.status(200).json(result);
     } catch (error) {
-      console.log(error);
       return res.status(400).json(error);
     }
   }
 
-
   return res
-      .status(405)
-      .json({ message: "Method or endpoint not implemented." });
+    .status(405)
+    .json({ message: "Method or endpoint not implemented." });
 }
