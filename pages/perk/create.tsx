566da101079ee1e1776242d21f499c9977079936
Nicolas Guillen <guillen_nicolas@live.com>
2022-07-23 00:21:49 -0500

    🔒 checks that user is the admin of a company

--- pages/perk/create.tsx
+++ pages/perk/create.tsx
@@ -1,18 +1,31 @@
 import type { NextPage } from "next";
-import { useMantineTheme} from "@mantine/core";
+import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { useMantineTheme, Text } from "@mantine/core";
 import AppPerkForm from "../../components/AppPerkForm";
 
-const CreatePerkPage: NextPage = () => {
+interface Props {
+  user: any;
+}
+
+const CreatePerkPage: NextPage<Props> = ({ user }) => {
   const theme = useMantineTheme();
 
   return (
-    <div style={{ minHeight: "100vh", marginBottom: "49px"  }}>
+    <div style={{ minHeight: "100vh", marginBottom: "49px" }}>
       <div style={{ padding: theme.spacing.md }}>
         <h2 style={{ marginBottom: 0 }}>Create Perk</h2>
       </div>
-      <AppPerkForm></AppPerkForm>
+      {user.adminOf ? (
+        <AppPerkForm></AppPerkForm>
+      ) : (
+        <div style={{ padding: theme.spacing.md }}>
+          <Text>You have to be a company admin to create a perk</Text>
+        </div>
+      )}
     </div>
   );
 };
 
 export default CreatePerkPage;
+
+export const getServerSideProps = withPageAuthRequired();
