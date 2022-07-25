6894f2eed46e3032766a712107113f5d42f1d67e
nicolascalev <nicolascalevg@gmail.com>
2022-07-25 12:04:32 -0500

    🚧 load offers, display, fix auto scroll, major changes

--- pages/company/index.tsx
+++ pages/company/index.tsx
@@ -8,6 +8,8 @@ import {
   Tabs,
   SimpleGrid,
   Alert,
+  Loader,
+  Center,
 } from "@mantine/core";
 import { useState, useEffect } from "react";
 import { useWindowScroll } from "react-use";
@@ -15,9 +17,10 @@ import { debounce } from "lodash";
 import { AlertCircle } from "tabler-icons-react";
 import { useRouter } from "next/router";
 import axios from "axios";
+import AppPerkCard from "components/AppPerkCard";
 
 const debounceScrollHandle = debounce((y, setShowCompanyInfo) => {
-  if (y <= 5) {
+  if (y <= 50) {
     setShowCompanyInfo(true);
   } else {
     setShowCompanyInfo(false);
@@ -43,41 +46,51 @@ const Company: NextPage<Props> = ({ user }) => {
   const router = useRouter();
   const { logoClass, showCompanyInfo } = useDisplayProfile();
 
-  const [loadingOffers, setLoadingOffers] = useState(false)
-  const [offers, setOffers] = useState([])
+  const [loadingOffers, setLoadingOffers] = useState(false);
+  const [offers, setOffers] = useState([]);
   useEffect(() => {
     async function loadOffers() {
-      setLoadingOffers(true)
+      setLoadingOffers(true);
       try {
-        const { data } = await axios.get(`/api/company/${user.company.id}/offers`)
-        console.log(data)
-        setOffers(data)
+        const { data } = await axios.get(
+          `/api/company/${user.company.id}/offers`
+        );
+        console.log(data);
+        setOffers(data);
       } catch (err) {
         // TODO: do something with the error
-        console.log(err)
+        console.log(err);
       } finally {
-        setLoadingOffers(false)
+        setLoadingOffers(false);
       }
     }
-    loadOffers()
-  }, [setLoadingOffers, setOffers, user.company.id])
+    loadOffers();
+  }, [setLoadingOffers, setOffers, user.company.id]);
+
+  const isDark = theme.colorScheme === "dark"
+  const logoContainerStyles: any = {
+    position: "sticky",
+    top: "50px",
+    display: "flex",
+    justifyContent: "center",
+    backgroundColor:
+      theme.colorScheme === "dark" ? theme.colors.dark[7] : theme.white,
+    zIndex: 10,
+    padding: theme.spacing.xs,
+  }
+  if (!showCompanyInfo) {
+    logoContainerStyles.borderBottom = "1px solid " + (isDark ? theme.colors.dark[5] : "#ced4da");
+  } 
 
   if (!user.company) {
     return <div>You have to be a part of a company or create one</div>;
   }
 
   return (
-    <div style={{ position: "relative", height: "200vh" }}>
+    <div style={{ position: "relative", minHeight: "100vh", marginBottom: "49px" }}>
       {user.company.logo && (
         <div
-          style={{
-            position: "sticky",
-            top: "52px",
-            display: "flex",
-            justifyContent: "center",
-            backgroundColor:
-              theme.colorScheme === "dark" ? theme.colors.dark[7] : theme.white,
-          }}
+          style={logoContainerStyles}
         >
           {/* this div reduces impact of stats on scroll for now */}
           <div
@@ -99,82 +112,87 @@ const Company: NextPage<Props> = ({ user }) => {
           ></Image>
         </div>
       )}
-
-      {showCompanyInfo && (
-        <div
-          style={{
-            padding: theme.spacing.md,
-            display: "flex",
-            flexWrap: "wrap",
-          }}
-        >
-          <div style={{ width: "100%", textAlign: "center" }}>
-            <Text size="xl" weight={500}>
-              {user.company.name}
-            </Text>
-            <Paper
-              radius="md"
-              p="md"
-              style={{
-                backgroundColor:
-                  theme.colorScheme === "dark"
-                    ? theme.colors.dark[6]
-                    : theme.colors.gray[0],
-                marginTop: theme.spacing.md,
-              }}
-            >
-              <SimpleGrid cols={3}>
-                <div>
-                  <Text weight="bold" size="lg">
-                    500
-                  </Text>
-                  <Text>Perks</Text>
-                </div>
-                <div>
-                  <Text weight="bold" size="lg">
-                    {offers.length}
-                  </Text>
-                  <Text>Offers</Text>
-                </div>
-                <div>
-                  <Text weight="bold" size="lg">
-                    1000
-                  </Text>
-                  <Text>Claims</Text>
-                </div>
-              </SimpleGrid>
-            </Paper>
-          </div>
-
-          {user.adminOf && user.company?.paidMembership == false && (
-            <Alert
-              icon={<AlertCircle size={16} />}
-              title="Subscription"
-              color="yellow"
-              radius="md"
-              style={{ marginTop: theme.spacing.md, position: "initial" }}
-              onClick={()=> router.push("/subscription")}
-            >
-              You need to get a subscription to show offers and get perks, click
-              here
-            </Alert>
-          )}
-        </div>
-      )}
       <div
         style={{
-          position: "sticky",
-          top: "89px",
           padding: theme.spacing.md,
+          display: "flex",
+          flexWrap: "wrap",
         }}
+        className={
+          showCompanyInfo
+            ? "company__info"
+            : "company__info company__info_hidden"
+        }
       >
+        <div style={{ width: "100%", textAlign: "center" }}>
+          <Text size="xl" weight={500}>
+            {user.company.name}
+          </Text>
+          <Paper
+            radius="md"
+            p="md"
+            style={{
+              backgroundColor:
+                theme.colorScheme === "dark"
+                  ? theme.colors.dark[6]
+                  : theme.colors.gray[0],
+              marginTop: theme.spacing.md,
+            }}
+          >
+            <SimpleGrid cols={3}>
+              <div>
+                <Text weight="bold" size="lg">
+                  500
+                </Text>
+                <Text>Perks</Text>
+              </div>
+              <div>
+                <Text weight="bold" size="lg">
+                  {offers.length}
+                </Text>
+                <Text>Offers</Text>
+              </div>
+              <div>
+                <Text weight="bold" size="lg">
+                  1000
+                </Text>
+                <Text>Claims</Text>
+              </div>
+            </SimpleGrid>
+          </Paper>
+        </div>
+
+        {user.adminOf && user.company?.paidMembership == false && (
+          <Alert
+            icon={<AlertCircle size={16} />}
+            title="Subscription"
+            color="yellow"
+            radius="md"
+            style={{ marginTop: theme.spacing.md, position: "initial" }}
+            onClick={() => router.push("/subscription")}
+          >
+            You need to get a subscription to show offers and get perks, click
+            here
+          </Alert>
+        )}
+      </div>
+      <div>
         <Tabs grow variant="pills" position="apart" color="primary">
           {/* TODO: show public and private just add a filter somewhere OR add the private instead of popular for mvp given that popular might be hard to calculate and make performant */}
+          {/* TODO: upgrade to mantine v5 and change bg color on tab content for light theme */}
           <Tabs.Tab label="Newest">
             <div>First tab content</div>
           </Tabs.Tab>
           <Tabs.Tab label="Offers">
-
+            {loadingOffers ? (
+              <Center>
+                <Loader />
+              </Center>
+            ) : (
+              offers.map((offer: any) => (
+                <AppPerkCard key={offer.id} perk={offer}></AppPerkCard>
+              ))
+            )}
           </Tabs.Tab>
           <Tabs.Tab label="About">
             <Text>{user.company.about}</Text>
