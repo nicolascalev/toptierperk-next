8f7eb9dd27ae7696f20beff2ffdb5229ec457881
Nicolas Guillen <guillen_nicolas@live.com>
2022-07-28 13:19:31 -0500

    🔒 add policy guard, available only for admin of

--- pages/perk/[id]/update.tsx
+++ pages/perk/[id]/update.tsx
@@ -1,14 +1,16 @@
 import { useState, useEffect } from "react";
 import type { NextPage } from "next";
-import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { withPageAuthRequired, getSession } from "@auth0/nextjs-auth0";
 import { useRouter } from "next/router";
 import { useMantineTheme, Text, Loader, Center } from "@mantine/core";
 import AppPerkForm from "components/AppPerkForm";
 import axios from "axios";
-import Error from 'next/error'
+import Error from "next/error";
+import Benefit from "prisma/models/Benefit";
 
 interface Props {
   user: any;
+  serverError: any;
 }
 
 function useFetchPerk(perkId: string) {
@@ -37,21 +39,27 @@ function useFetchPerk(perkId: string) {
   };
 }
 
-const UpdatePerkPage: NextPage<Props> = ({ user }) => {
+const UpdatePerkPage: NextPage<Props> = ({ user, serverError }) => {
   const theme = useMantineTheme();
   const router = useRouter();
-  const {
-    loadingPerk,
-    perk,
-    loadPerkError,
-  } = useFetchPerk(router.query.id as string);
+  const { loadingPerk, perk, loadPerkError } = useFetchPerk(
+    router.query.id as string
+  );
+
+  if (serverError) {
+    return (<Error statusCode={serverError}></Error>)
+  }
 
   return (
     <div style={{ minHeight: "100vh", marginBottom: "49px" }}>
       <div style={{ padding: theme.spacing.md }}>
         <h2 style={{ marginBottom: 0 }}>Update Perk</h2>
       </div>
-      {loadingPerk && <Center><Loader></Loader></Center>}
+      {loadingPerk && (
+        <Center>
+          <Loader></Loader>
+        </Center>
+      )}
       {loadPerkError && <Error statusCode={loadPerkError.status}></Error>}
       {/* TODO: pass perk to AppPerkForm for update */}
       {perk && !loadPerkError && (
@@ -63,5 +71,22 @@ const UpdatePerkPage: NextPage<Props> = ({ user }) => {
 
 export default UpdatePerkPage;
 
-// TODO add auth guard on server side so the perk info is not fetched, adminOf should be perk.suplier.id
-export const getServerSideProps = withPageAuthRequired();
+export const getServerSideProps = withPageAuthRequired({
+  async getServerSideProps(ctx) {
+    try {
+      const session = getSession(ctx.req, ctx.res);
+      const perkId = Number(ctx.query.id);
+      const perk = await Benefit.findById(perkId);
+      if (!perk) {
+        return { props: { serverError: 404 } }
+      }
+      if (perk.supplier.id !== session!.user.adminOfId) {
+        return { props: { serverError: 403 } };
+      }
+  
+      return { props: { serverError: 0 } };
+    } catch (err) {
+      throw err;
+    }
+  },
+});
