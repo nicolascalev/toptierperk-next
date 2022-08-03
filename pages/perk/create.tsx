4e3f2c47adb235ba7c2a08ec050426c33c1ed26c
nicolascalev <nicolascalevg@gmail.com>
2022-08-03 00:03:16 -0500

    🎨 display form only if company has paid sub

--- pages/perk/create.tsx
+++ pages/perk/create.tsx
@@ -1,18 +1,46 @@
 import type { NextPage } from "next";
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
-import { Text, Box } from "@mantine/core";
+import { Text, Box, Alert, useMantineTheme, Anchor } from "@mantine/core";
 import AppPerkForm from "components/AppPerkForm";
+import { AlertCircle } from "tabler-icons-react";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 interface Props {
   user: any;
 }
 
 const CreatePerkPage: NextPage<Props> = ({ user }) => {
+  const theme = useMantineTheme();
+  const router = useRouter();
+
+  if (user.adminOf && user.company?.paidMembership == false) {
+    return (
+      <Box p="md">
+        <Alert
+          icon={<AlertCircle size={16} />}
+          title="Subscription"
+          color="yellow"
+          radius="md"
+          style={{ marginTop: theme.spacing.md, position: "initial" }}
+          onClick={() => router.push("/subscription")}
+        >
+          You need to
+          <Link href="/subscription" passHref>
+            <Anchor component="a"> get a subscription </Anchor>
+          </Link>
+          to create perks and offers.
+        </Alert>
+      </Box>
+    );
+  }
 
   return (
     <div style={{ minHeight: "100vh", marginBottom: "49px" }}>
       <Box p="md">
-        <Text size="xl" weight={500}>Create perk</Text>
+        <Text size="xl" weight={500}>
+          Create perk
+        </Text>
       </Box>
       {user.adminOf ? (
         <AppPerkForm action="create"></AppPerkForm>
