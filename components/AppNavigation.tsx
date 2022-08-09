025bc6606db56832bb25dad1f214910b53d69123
nicolascalev <nicolascalevg@gmail.com>
2022-08-08 21:40:33 -0600

    🐛 use app navigation and user in session for navigation

---
+++ components/AppNavigation.tsx
@@ -0,0 +1,60 @@
+import { ScrollArea, Box, NavLink } from "@mantine/core";
+import Link from "next/link";
+import { BuildingSkyscraper, Scan, Logout, Login } from "tabler-icons-react";
+import { useRouter } from "next/router";
+import { useUser } from "@auth0/nextjs-auth0";
+
+function AppNavigation(props: any) {
+  const router = useRouter();
+  const { user } = useUser();
+  return (
+    <ScrollArea style={{ height: 282 }}>
+      <Box>
+        {user && (
+          <>
+            <Link href="/company/admin" passHref>
+              <NavLink
+                component="a"
+                variant="subtle"
+                label="Your company"
+                icon={<BuildingSkyscraper size={14} />}
+                active={router.pathname == "/company/admin"}
+              />
+            </Link>
+            <Link href="/scan/costumer" passHref>
+              <NavLink
+                component="a"
+                variant="subtle"
+                label="Verify Costumer QR"
+                icon={<Scan size={14} />}
+                active={router.pathname == "/scan/costumer"}
+              />
+            </Link>
+            <Link href="/api/auth/logout" passHref>
+              <NavLink
+                variant="filled"
+                color="red"
+                component="a"
+                label="Logout"
+                icon={<Logout size={14} />}
+              />
+            </Link>
+          </>
+        )}
+        {!user && (
+          <Link href="/api/auth/login" passHref>
+            <NavLink
+              variant="subtle"
+              component="a"
+              label="Login"
+              icon={<Login size={14} />}
+            />
+          </Link>
+        )}
+        {props.children}
+      </Box>
+    </ScrollArea>
+  );
+}
+
+export default AppNavigation;
