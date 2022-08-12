1c7ad54bfc73d44950fd9d0e8b171ff98dfc70e0
nicolascalev <nicolascalevg@gmail.com>
2022-08-12 11:48:30 -0600

    🚧 allow employees to query this endpoint

--- pages/api/company/[companyId]/benefits.tsx
+++ pages/api/company/[companyId]/benefits.tsx
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import Benefit, { BenefitSearchParams } from "prisma/models/Benefit"
+import Benefit, { BenefitSearchParams } from "prisma/models/Benefit";
 import { getSession } from "@auth0/nextjs-auth0";
 import isAuthenticated from "helpers/isAuthenticated";
 
@@ -11,30 +11,42 @@ export default async function findCompanyBenfits(
     await isAuthenticated(req, res);
     let session = getSession(req, res);
 
+    // TODO: move the employee query to another endpoint, leave this for admin only
     const companyId = Number(req.query.companyId);
-    if (session!.user.adminOfId !== companyId) {
-      return res.redirect('/403')
+    if (
+      session!.user.adminOfId !== companyId &&
+      session!.user.company?.id !== companyId
+    ) {
+      return res.redirect("/403");
     }
 
-    const q = req.query
+    const q = req.query;
     const params: BenefitSearchParams = {
       beneficiaryId: companyId,
-      searchString: q.searchString as string || undefined,
+      searchString: (q.searchString as string) || undefined,
       skip: q.skip ? Number(q.skip) : undefined,
       take: q.take ? Number(q.take) : undefined,
       cursor: q.cursor ? Number(q.cursor) : undefined,
       category: q.category ? Number(q.category) : undefined,
-      privacy: q.privacy ? q.privacy == 'true' : q.privacy == 'false' ? false : undefined,
-      acquired: q.acquired ? q.acquired == 'true' : q.aquired == 'false' ? false : undefined,
-      startsAt: q.startsAt as string || undefined,
-    }
-    
+      privacy: q.privacy
+        ? q.privacy == "true"
+        : q.privacy == "false"
+        ? false
+        : undefined,
+      acquired: q.acquired
+        ? q.acquired == "true"
+        : q.aquired == "false"
+        ? false
+        : undefined,
+      startsAt: (q.startsAt as string) || undefined,
+    };
+
     try {
-      const perks = await Benefit.adminBenefitSearch(params)
-      return res.status(200).json(perks)
+      const perks = await Benefit.adminBenefitSearch(params);
+      return res.status(200).json(perks);
     } catch (error) {
-      console.log(error)
-      return res.status(500).json({ error })
+      console.log(error);
+      return res.status(500).json({ error });
     }
   }
 
