641c8e96cd2563718a5c8a1eff9a4999add77956
Nicolas Guillen <guillen_nicolas@live.com>
2022-08-12 23:51:12 -0600

    🐜 native share and copy url

--- components/AppPerkViewActions.tsx
+++ components/AppPerkViewActions.tsx
@@ -10,12 +10,19 @@ import {
   Drawer,
   NavLink,
 } from "@mantine/core";
-import { Bookmark, DotsVertical, Share, Link as LinkIcon, MessageReport  } from "tabler-icons-react";
+import {
+  Bookmark,
+  DotsVertical,
+  Share,
+  Link as LinkIcon,
+  MessageReport,
+} from "tabler-icons-react";
+import { showNotification } from "@mantine/notifications";
 
 type Props = {
-  perkName: string;
+  perk: any;
 };
-function AppPerkViewActions({ perkName }: Props) {
+function AppPerkViewActions({ perk }: Props) {
   const theme = useMantineTheme();
   const isDark = theme.colorScheme === "dark";
   const componentRef = useRef<any>(null);
@@ -28,6 +35,51 @@ function AppPerkViewActions({ perkName }: Props) {
       setActionsHeight(componentRef.current.offsetHeight);
     }
   }, []);
+
+  function clickShare() {
+    const shareData = {
+      title: perk.name,
+      text: "Toptierperk, making B2B perks available for everyone",
+      url: window.location.origin + `/perk${perk.id}`,
+    };
+    navigator
+      .share(shareData)
+      .then(() => {
+        showNotification({
+          title: "Shared",
+          message: "Thanks for sharing this perk!",
+          autoClose: 3000,
+        });
+      })
+      .catch(() => {
+        showNotification({
+          title: "Not available",
+          message: "Sharing is not available for your browser",
+          autoClose: 3000,
+        });
+      })
+      .finally(() => setOpenedActions(false));
+  }
+
+  function clickCopyUrl() {
+    navigator.clipboard
+      .writeText(window.location.origin + `/perk${perk.id}`)
+      .then(() => {
+        showNotification({
+          title: "Copied",
+          message: "Url copied to keyboard",
+          autoClose: 3000,
+        });
+      })
+      .catch(() => {
+        showNotification({
+          title: "Oops",
+          message: "Could not copy to keyboard",
+          autoClose: 3000,
+        });
+      })
+      .finally(() => setOpenedActions(false));
+  }
 
   // TODO: get errors or validations from props, and disable claim or actions
   return (
@@ -44,9 +96,12 @@ function AppPerkViewActions({ perkName }: Props) {
       >
         <Divider />
         <Group p="md" align="top" noWrap spacing="xs">
-          <Text sx={{flexGrow: 1}}>{perkName}</Text>
+          <Text sx={{ flexGrow: 1 }}>{perk.name}</Text>
           <Button>Claim</Button>
-          <ActionIcon variant="transparent" onClick={() => setOpenedActions(true)}>
+          <ActionIcon
+            variant="transparent"
+            onClick={() => setOpenedActions(true)}
+          >
             <DotsVertical />
           </ActionIcon>
         </Group>
@@ -61,8 +116,16 @@ function AppPerkViewActions({ perkName }: Props) {
       >
         <Box>
           <NavLink label="Save" icon={<Bookmark size={16} />} />
-          <NavLink label="Share" icon={<Share size={16} />} />
-          <NavLink label="Copy url" icon={<LinkIcon size={16} />} />
+          <NavLink
+            label="Share"
+            icon={<Share size={16} />}
+            onClick={clickShare}
+          />
+          <NavLink
+            label="Copy url"
+            icon={<LinkIcon size={16} />}
+            onClick={clickCopyUrl}
+          />
           <NavLink label="Report abuse" icon={<MessageReport size={16} />} />
         </Box>
       </Drawer>
