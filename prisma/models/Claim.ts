743db720f665d0823848b1888fd94a58a9dffc69
Nicolas Guillen <guillen_nicolas@live.com>
2022-08-16 00:38:40 -0600

    🦍 include supplier

--- prisma/models/Claim.ts
+++ prisma/models/Claim.ts
@@ -2,7 +2,7 @@ import { Prisma } from "@prisma/client";
 import prisma from "prisma/prisma.client";
 
 const Claim = {
-  async create(userId: number, benefitId: number, businessId: number) {
+  async create(userId: number, benefitId: number, businessId: number, supplierId: number) {
     try {
       const claim = await prisma.claim.create<Prisma.ClaimCreateArgs>({
         data: {
@@ -15,6 +15,34 @@ const Claim = {
           business: {
             connect: { id: businessId },
           },
+          supplier: {
+            connect: { id: supplierId },
+          },
+        },
+      });
+      return claim;
+    } catch (err) {
+      return Promise.reject(err);
+    }
+  },
+
+  async findById(claimId: number) {
+    try {
+      const claim = await prisma.claim.findFirst<Prisma.ClaimFindFirstArgs>({
+        where: {
+          id: claimId,
+        },
+        include: {
+          user: { include: { picture: true } },
+          benefit: {
+            include: {
+              photos: true,
+              supplier: { include: { logo: true } },
+              categories: true,
+            },
+          },
+          business: { include: { logo: true } },
+          supplier: { include: { logo: true } },
         },
       });
       return claim;
