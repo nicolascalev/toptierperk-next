7128af4477578a3e6f27add2423aa6ea00d8418a
nicolascalev <nicolascalevg@gmail.com>
2022-08-15 18:39:25 -0600

    🚧 do some validations and create claim

--- components/AppPerkViewActions.tsx
+++ components/AppPerkViewActions.tsx
@@ -18,13 +18,41 @@ import {
   MessageReport,
 } from "tabler-icons-react";
 import { showNotification } from "@mantine/notifications";
+import { useRouter } from "next/router";
+import axios from "axios";
+import useSWR from "swr";
+import confetti from "canvas-confetti";
+
+const fetcher = (url: string) => axios.get(url).then((res) => res.data);
+
+function useAcquiredStatus(businessId?: number, benefitId?: number) {
+  const { data, error } = useSWR(
+    businessId && benefitId
+      ? `/api/business/${businessId}/benefits/${benefitId}`
+      : null,
+    fetcher
+  );
+  const loading = !data && !error && businessId && benefitId ? true : false;
+
+  return {
+    acquireStatus: data,
+    loadingAcquireStatus: loading,
+  };
+}
 
 type Props = {
   perk: any;
+  user: any;
+  initialError: string;
 };
-function AppPerkViewActions({ perk }: Props) {
+function AppPerkViewActions({ perk, user, initialError }: Props) {
+  const router = useRouter();
   const theme = useMantineTheme();
   const isDark = theme.colorScheme === "dark";
+  const { acquireStatus, loadingAcquireStatus } = useAcquiredStatus(
+    user?.business.id,
+    perk.id
+  );
   const componentRef = useRef<any>(null);
   const [actionsHeight, setActionsHeight] = useState(49);
 
@@ -81,6 +109,82 @@ function AppPerkViewActions({ perk }: Props) {
       .finally(() => setOpenedActions(false));
   }
 
+  function onClickClaim() {
+    if (!user) {
+      return router.push("/api/auth/login");
+    }
+    if (!user.emailVerified) {
+      return showNotification({
+        title: "Not allowed",
+        message: "You need to verify your email address first",
+        autoClose: 3000,
+      });
+    }
+    if (!user.business) {
+      return showNotification({
+        title: "Not allowed",
+        message: "You must belong to a business first to claim any perk",
+        autoClose: 3000,
+      });
+    }
+    if (!acquireStatus?.perkIsAcquired) {
+      return showNotification({
+        title: "Not allowed",
+        message: "Your employeer has not acquired this perk",
+        autoClose: 3000,
+      });
+    }
+    if (initialError) {
+      return showNotification({
+        title: "Not allowed",
+        message: initialError,
+        autoClose: 3000,
+      });
+    }
+    claimPerk(perk.id);
+  }
+
+  const [loadingClaim, setLoadingClaim] = useState(false);
+  async function claimPerk(benefitId: number) {
+    try {
+      setLoadingClaim(true);
+      const claim = await axios
+        .post(`/api/benefit/${benefitId}/claim`)
+        .then((res) => res.data);
+      if (claim) {
+        console.log(claim);
+        confetti({
+          particleCount: 100,
+          spread: 70,
+          origin: { y: 0.6 },
+        });
+        showNotification({
+          title: "Congratulations 🎉",
+          message: "you have to show the qr to the supplier to verify claim",
+          autoClose: 5000,
+        });
+        // router.push("/claim/" + claim.id)
+      }
+    } catch (err) {
+      let error = err as any;
+      if (error?.response?.data?.code === "E_NOT_ALLOWED") {
+        showNotification({
+          title: "Not allowed",
+          message: error.response.data.message,
+          autoClose: 5000,
+        });
+        return;
+      }
+      showNotification({
+        title: "Please try again",
+        message: "There was an error",
+        autoClose: 5000,
+      });
+    } finally {
+      setLoadingClaim(false);
+    }
+  }
+
   // TODO: get errors or validations from props, and disable claim or actions
   return (
     <>
@@ -94,10 +198,27 @@ function AppPerkViewActions({ perk }: Props) {
           backgroundColor: isDark ? theme.colors.dark[7] : "white",
         }}
       >
+        <div
+          style={{
+            position: "absolute",
+            top: "-1.5rem",
+            left: "0px",
+            width: "100%",
+            height: "1.5rem",
+            backgroundImage: `linear-gradient(transparent, ${
+              isDark ? theme.colors.dark[7] : "white"
+            })`,
+          }}
+        ></div>
         <Divider />
         <Group p="md" align="top" noWrap spacing="xs">
           <Text sx={{ flexGrow: 1 }}>{perk.name}</Text>
-          <Button>Claim</Button>
+          <Button
+            loading={loadingAcquireStatus || loadingClaim}
+            onClick={onClickClaim}
+          >
+            Claim
+          </Button>
           <ActionIcon
             variant="transparent"
             onClick={() => setOpenedActions(true)}
