16c311fa971378b3dd97469a3c8bbc14a9c93ab2
nicolascalev <nicolascalevg@gmail.com>
2022-08-16 22:56:05 -0600

    🔒 allow verifiers to read claim

--- pages/api/claim/[claimId].ts
+++ pages/api/claim/[claimId].ts
@@ -2,6 +2,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import isAuthenticated from "helpers/isAuthenticated";
 import { getSession } from "@auth0/nextjs-auth0";
 import Claim from "prisma/models/Claim";
+import type { User } from "@prisma/client";
 
 export default async function claimHandler(
   req: NextApiRequest,
@@ -11,13 +12,18 @@ export default async function claimHandler(
     try {
       await isAuthenticated(req, res);
       let session = getSession(req, res);
+      const user: User = session!.user as User;
 
       const claimId = Number(req.query.claimId);
       const claim = await Claim.findById(claimId);
       if (!claim) {
         return res.status(404).send("Not Found");
       }
-      if (claim.userId !== session!.user.id) {
+      const userIsClaimer = claim.userId === user.id;
+      // if user can verify and belongs to perk supplier business
+      const userIsVerifierOfSupplier =
+        user.canVerify && user.businessId === claim.supplierId ? true : false;
+      if (!userIsClaimer && !userIsVerifierOfSupplier) {
         return res.status(403).send("Forbidden");
       }
       return res.status(200).json(claim);
