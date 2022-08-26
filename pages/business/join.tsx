5984dc9868b9603118786d598097b40155a546cc
nicolascalev <nicolascalevg@gmail.com>
2022-08-26 15:32:28 -0600

    🦍 join business functionality ready!

--- pages/business/join.tsx
+++ pages/business/join.tsx
@@ -11,6 +11,8 @@ import {
   Button,
   Divider,
   Image,
+  Center,
+  Loader,
 } from "@mantine/core";
 import { ChevronRight, UserX } from "tabler-icons-react";
 import Error from "next/error";
@@ -22,6 +24,7 @@ import axios from "axios";
 import useSWR from "swr";
 import refreshSessionUser from "helpers/refreshSessionUser";
 import { useDebouncedValue } from "@mantine/hooks";
+import { useRouter } from "next/router";
 
 const fetcher = (url: string, params: any) =>
   axios.get(url, { params }).then((res) => res.data);
@@ -32,10 +35,12 @@ interface Props {
 }
 
 const JoinBusinessView: NextPage<Props> = ({ user, serverError }) => {
+  const router = useRouter();
   const theme = useMantineTheme();
   const isDark = theme.colorScheme === "dark";
   const [animationParent] = useAutoAnimate<HTMLDivElement>();
   const listBackground = isDark ? theme.colors.dark[8] : theme.colors.gray[1];
+  const filterBackground = isDark ? theme.colors.dark[7] : theme.white;
 
   const [searchString, setSearchString] = useState("");
   const [debSearchString] = useDebouncedValue(searchString, 500);
@@ -80,6 +85,53 @@ const JoinBusinessView: NextPage<Props> = ({ user, serverError }) => {
     setSkip(1);
   }
 
+  function onClickJoin(business: any) {
+    const allowedEmails = JSON.parse(business.allowedEmployeeEmails);
+    if (!allowedEmails.includes(user.email)) {
+      showNotification({
+        title: "Not allowed",
+        message:
+          "Your email is not in the allowed email list for this business",
+        color: "red",
+        autoClose: 5000,
+      });
+      return;
+    }
+    joinBusiness(business.id);
+  }
+
+  const [loadingJoin, setLoadingJoin] = useState(false);
+  const [joiningBusinessId, setJoiningBusinessId] = useState<null | number>(
+    null
+  );
+  async function joinBusiness(businessId: number) {
+    try {
+      setJoiningBusinessId(businessId);
+      setLoadingJoin(true);
+      await axios
+        .patch(`/api/user/${user.id}/business/${businessId}`)
+        .then((res) => res.data);
+      showNotification({
+        title: "Joined business",
+        message: "You will be redirected to business page in 3 seconds",
+        autoClose: 3000,
+      });
+      setTimeout(() => {
+        router.push("/business");
+      }, 3000);
+    } catch (err) {
+      showNotification({
+        title: "Request failed",
+        message: "We could not process your request, please try again",
+        color: "red",
+        autoClose: 5000,
+      });
+    } finally {
+      setJoiningBusinessId(null);
+      setLoadingJoin(false);
+    }
+  }
+
   return (
     <>
       {serverError ? (
@@ -87,11 +139,19 @@ const JoinBusinessView: NextPage<Props> = ({ user, serverError }) => {
       ) : (
         <Box sx={{ marginBottom: "49px" }}>
           <AppHeaderTitle title="Join Business" />
-          <Box p="md">
+          <Box
+            p="md"
+            style={{
+              position: "sticky",
+              top: "49px",
+              backgroundColor: filterBackground,
+              zIndex: 10,
+            }}
+          >
             <TextInput
               value={searchString}
               onChange={(e) => setSearchString(e.currentTarget.value)}
-              placeholder="Find by name"
+              placeholder="Find business by name"
             />
           </Box>
 
@@ -116,15 +176,36 @@ const JoinBusinessView: NextPage<Props> = ({ user, serverError }) => {
                       alt={business.name}
                     />
                     <div style={{ flexGrow: 1 }}>
-                      <Text>{business.name}</Text>
-                      <Text color="dimmed" size="sm">{business.email}</Text>
+                      <Text style={{ width: "100%", wordBreak: "break-word" }}>
+                        {business.name}
+                      </Text>
+                      <Text
+                        color="dimmed"
+                        size="sm"
+                        style={{ width: "100%", wordBreak: "break-word" }}
+                      >
+                        {business.email}
+                      </Text>
                     </div>
-                    <Button variant="subtle">Join</Button>
+                    <Button
+                      variant="subtle"
+                      onClick={() => onClickJoin(business)}
+                      loading={loadingJoin && business.id === joiningBusinessId}
+                      disabled={loadingJoin}
+                    >
+                      Join
+                    </Button>
                   </Group>
                   <Divider />
                 </div>
               ))}
             </div>
+            {/* loader for initial load */}
+            {loadingBusinesses && businesses.length === 0 && (
+              <Center>
+                <Loader size="sm" variant="bars"></Loader>
+              </Center>
+            )}
             {/* no results with no search string */}
             {!loadingBusinesses && businesses.length === 0 && !debSearchString && (
               <Paper withBorder p="md" mb="md">
@@ -148,14 +229,17 @@ const JoinBusinessView: NextPage<Props> = ({ user, serverError }) => {
                 </Text>
               </Paper>
             )}
-            <Button
-              fullWidth
-              loading={loadingBusinesses}
-              disabled={!theresMore}
-              onClick={loadMore}
-            >
-              {theresMore ? "Load more" : "Up to date"}
-            </Button>
+            {businesses.length > 0 && (
+              <Button
+                mt="md"
+                fullWidth
+                loading={loadingBusinesses}
+                disabled={!theresMore}
+                onClick={loadMore}
+              >
+                {theresMore ? "Load more" : "Up to date"}
+              </Button>
+            )}
           </Box>
         </Box>
       )}
@@ -177,9 +261,9 @@ export const getServerSideProps = withPageAuthRequired({
       };
     }
     const session = getSession(ctx.req, ctx.res);
-    if (!session?.user.adminOf) {
-      return { props: { serverError: 401 } };
-    }
+    // if (session?.user.businessId) {
+    //   return { redirect: { destination: "/business", permanent: false } };
+    // }
     return { props: { serverError: 0 } };
   },
 });
