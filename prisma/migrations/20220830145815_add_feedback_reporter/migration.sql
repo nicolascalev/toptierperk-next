121be754e99e2f98e40a81e31f4b114e68d2ffb8
nicolascalev <nicolascalevg@gmail.com>
2022-08-30 08:58:40 -0600

    🗃 add reporter to feedback

---
+++ prisma/migrations/20220830145815_add_feedback_reporter/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - Added the required column `reporterId` to the `Feedback` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE `feedback` ADD COLUMN `reporterId` INTEGER NOT NULL;
+
+-- AddForeignKey
+ALTER TABLE `Feedback` ADD CONSTRAINT `Feedback_reporterId_fkey` FOREIGN KEY (`reporterId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
