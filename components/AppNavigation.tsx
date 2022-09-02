58c61df6d4291408ac25e2b19caa2d4f02d83e15
nicolascalev <nicolascalevg@gmail.com>
2022-09-01 18:46:36 -0600

    🎨 admin, scanner, available to admin and verifier

--- components/AppNavigation.tsx
+++ components/AppNavigation.tsx
@@ -1,35 +1,54 @@
 import { ScrollArea, Box, NavLink } from "@mantine/core";
 import Link from "next/link";
-import { BuildingSkyscraper, Scan, Logout, Login } from "tabler-icons-react";
+import {
+  BuildingSkyscraper,
+  Scan,
+  Logout,
+  Login,
+  MessageReport,
+} from "tabler-icons-react";
 import { useRouter } from "next/router";
 import { useUser } from "@auth0/nextjs-auth0";
+import { useState } from "react";
+import AppFeedbackDrawer from "./AppFeedbackDrawer";
 
 function AppNavigation(props: any) {
   const router = useRouter();
   const { user } = useUser();
+  const [openedFeedbackDrawer, setOpenedFeedbackDrawer] = useState(false);
   return (
     <ScrollArea style={{ height: 282 }}>
       <Box>
         {user && (
           <>
-            <Link href="/business/admin" passHref>
-              <NavLink
-                component="a"
-                variant="subtle"
-                label="Your business"
-                icon={<BuildingSkyscraper size={14} />}
-                active={router.pathname == "/business/admin"}
-              />
-            </Link>
-            <Link href="/scan/claim" passHref>
-              <NavLink
-                component="a"
-                variant="subtle"
-                label="Verify Claim QR"
-                icon={<Scan size={14} />}
-                active={router.pathname == "/scan/claim"}
-              />
-            </Link>
+            {user.adminOfId && (
+              <Link href="/business/admin" passHref>
+                <NavLink
+                  component="a"
+                  variant="subtle"
+                  label="Business admin"
+                  icon={<BuildingSkyscraper size={14} />}
+                  active={router.pathname == "/business/admin"}
+                />
+              </Link>
+            )}
+            {(user.adminOfId || user.canVerify) && (
+              <Link href="/scan/claim" passHref>
+                <NavLink
+                  component="a"
+                  variant="subtle"
+                  label="Verify costumer QR"
+                  icon={<Scan size={14} />}
+                  active={router.pathname == "/scan/claim"}
+                />
+              </Link>
+            )}
+            <NavLink
+              variant="subtle"
+              label="Report issue / feedback"
+              icon={<MessageReport size={14} />}
+              onClick={() => setOpenedFeedbackDrawer(true)}
+            />
             <Link href="/api/auth/logout" passHref>
               <NavLink
                 variant="filled"
@@ -53,6 +72,13 @@ function AppNavigation(props: any) {
         )}
         {props.children}
       </Box>
+      <AppFeedbackDrawer
+        opened={openedFeedbackDrawer}
+        onClose={() => setOpenedFeedbackDrawer(false)}
+        onFeedbackCreate={() => setOpenedFeedbackDrawer(false)}
+        size="full"
+        position="right"
+      />
     </ScrollArea>
   );
 }
