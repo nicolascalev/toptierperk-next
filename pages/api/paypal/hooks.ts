085023421471857b7e38c2de1cd6d3c34c43195f
nicolascalev <nicolascalevg@gmail.com>
2022-09-06 19:58:14 -0600

    🚧 update business on certain event types

--- pages/api/paypal/hooks.ts
+++ pages/api/paypal/hooks.ts
@@ -1,13 +1,31 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import Business from "prisma/models/Business";
 
 export default async function paypalHandler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  console.log("CALLED PAYPAL ROUTE");
+  console.log("CALLED PAYPAL HOOK ROUTE");
+  const triggerCancelEvents = [
+    "BILLING.SUBSCRIPTION.EXPIRED",
+    "BILLING.SUBSCRIPTION.CANCELLED",
+    "BILLING.SUBSCRIPTION.SUSPENDED",
+    "BILLING.SUBSCRIPTION.PAYMENT.FAILED",
+  ];
   if (req.method === "POST") {
-    console.log(req.body);
-    return res.status(200).json(req.body);
+    const paypalSubscriptionId = req.body.resource.id;
+    // TODO: i dont know if 
+    const subscriptionEndsAt = req.body.resource.billing_info.next_billing_time;
+    const eventType = req.body.event_type;
+    if (!triggerCancelEvents.includes(eventType)) {
+      return res.status(200).send("Nothing happened");
+    }
+
+    await Business.paypalUpdateSubscription(
+      paypalSubscriptionId,
+      subscriptionEndsAt
+    );
+    return res.status(200).send("Updated");
   }
   return res
     .status(405)
